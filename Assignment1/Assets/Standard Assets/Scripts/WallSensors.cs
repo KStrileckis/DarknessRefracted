2bd91a10b5d34d09212aac466acd4927f5fbfb4b
Kevin Strileckis <KStrileckis@gmail.com>
2015-02-16 13:30:13 -0500

    More edits. Should be clean and functional now. -KS

--- Assignment1/Assets/Standard Assets/Scripts/WallSensors.cs
+++ Assignment1/Assets/Standard Assets/Scripts/WallSensors.cs
@@ -6,19 +6,20 @@ public class WallSensors : MonoBehaviour {
 	public float senseRange;
 	public RaycastHit[] sensors = new RaycastHit[3];
 	public float[] distanceToWall = new float[3];
+	private float noWallVal;
 
 	// Use this for initialization
 	void Start () {
-
+		noWallVal = senseRange + 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//Reset wall distances. They are set to a value far beyond the senseRange as such a value is not possible unless there are no
 		//	walls within range.
-		distanceToWall [0] = senseRange+10;
-		distanceToWall [1] = senseRange+10;
-		distanceToWall [2] = senseRange+10;
+		distanceToWall [0] = noWallVal;
+		distanceToWall [1] = noWallVal;
+		distanceToWall [2] = noWallVal;
 
 		//Check if the sensors have hit anything
 			//NOTE: For the vector additions made, normalizing is not necessary as we are only using the sums for their  directions.
@@ -35,6 +36,14 @@ public class WallSensors : MonoBehaviour {
 			if(sensors[0].collider.CompareTag("Wall"))
 				distanceToWall[2] = sensors[2].distance;
 		}
+	}
+
+	//Function is used to get the distance to a wall by an outside function
+	public float GetDistanceToWall(int sensorIndex){
+		if(distanceToWall[sensorIndex] != noWallVal)
+			return distanceToWall [sensorIndex];
+
+		return noWallVal;
 	}
 
 
